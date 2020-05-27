5de419b8a501a34f98baf3aa0f1b8ff1fe9047e1
Pushan Paul <pushanpaul34@gmail.com>
2020-05-27 23:57:30 +0600

    New updates for Heroku deployment

--- pages/tests.py
+++ pages/tests.py
@@ -3,6 +3,7 @@ class SimpleTests(SimpleTestCase):
     def test_home_page_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+    
     def test_about_page_status_code(self):
         response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
